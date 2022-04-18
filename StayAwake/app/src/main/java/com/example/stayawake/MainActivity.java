97021185ca3c017a823b678598661781f0747e68
alu0101205953 <alu0101205953@ull.edu.es>
2022-04-18 21:53:46 +0100

    android setup

---
+++ StayAwake/app/src/main/java/com/example/stayawake/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.stayawake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import com.example.stayawake.WebAppInterface;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
+        webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
+        webView.loadUrl("file:///android_asset/html/index.html");
+    }
+}
